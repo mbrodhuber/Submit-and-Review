e2c87af46aa483981096ad398218c65a21a2dd74
mbrodhuber <35314964+mbrodhuber@users.noreply.github.com>
2024-09-25 20:44:07 +0000

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Submit-and-Review/',
   css: {
     postcss: './postcss.config.js',
   },
